2110ab4047509a4ccba0f7aff348b8b1e43c5001
VitaliyKlochko <89vklochko89@gmail.com>
2022-09-20 17:52:57 +0300

    Begin

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void changeValue() {
+        int value = 22;
+    }
+
+    public static void main(String[] args) {
+        int value = 33;
+        changeValue();
+        System.out.println(value);
+
+    }
+}
